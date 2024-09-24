ad7725455f796a5a033659f5cc582b0142a93e47
lakshmiym98 <lakshmikarthik9598@gmail.com>
2024-09-24 10:20:04 +0530

    my first commit

---
+++ com.kodbook/src/main/java/com/kodbook/controllers/PostController.java
@@ -0,0 +1,32 @@
+package com.kodbook.controllers;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kodbook.entities.Post;
+import com.kodbook.services.PostService;
+
+@Controller
+public class PostController {
+	@Autowired
+	PostService service;
+
+	@PostMapping("/createPost")
+	public String createPost(@RequestParam("caption") String caption, @RequestParam("photo") MultipartFile photo) {
+		Post post = new Post();
+		post.setCaption(caption);
+		try {
+			post.setPhoto(photo.getBytes());
+		} catch (IOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		service.createPost(post);
+		return "home";
+	}
+}
